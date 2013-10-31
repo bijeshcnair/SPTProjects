8ed94745482d931547f01513c17b104bad6edd81
bijeshcnair <bijeshcnair@gmail.com>
2013-10-31 19:43:37 +0530

    First test code .

--- src/main/java/com/bc/world/MyTestClass.java
+++ src/main/java/com/bc/world/MyTestClass.java
@@ -9,6 +9,7 @@ public class MyTestClass extends HttpServlet {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("the first test code. ");
 
 	}
 
