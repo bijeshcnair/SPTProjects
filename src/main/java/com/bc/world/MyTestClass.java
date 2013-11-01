3b96bc6e87dc641a208c26d84251e3aba99e7e52
bijeshcnair <bijeshcnair@gmail.com>
2013-11-01 14:16:44 +0530

    Added do post and do get method .

--- src/main/java/com/bc/world/MyTestClass.java
+++ src/main/java/com/bc/world/MyTestClass.java
@@ -1,6 +1,11 @@
 package com.bc.world;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class MyTestClass extends HttpServlet {
 
@@ -11,6 +16,18 @@ public class MyTestClass extends HttpServlet {
 		// TODO Auto-generated method stub
 		System.out.println("the first test code. ");
 
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
 	}
 
 }
